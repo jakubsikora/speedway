454346f17b8a2bb390ad1d0942ba98ca8e28304d
Jakub Sikora <jakub.sikora.en@gmail.com>
2015-06-10 08:01:35 +0100

    refactor

---
+++ public/js/bike.js
@@ -0,0 +1,11 @@
+function Bike() {
+  var forward = [0, 0]
+    , velXY = [0, 0]
+    , velocity = 0
+    , thrust = false
+    , clutch = false
+    , clutchFactor = 1
+    , angle = 0
+    , angleVel = 0
+    , sprite;
+}
---
+++ public/js/factors.js
@@ -0,0 +1,6 @@
+var FRICTION_FACTOR = 0.04
+  , TURN_ANGLE = 0.098172
+  , MAX_CLUTCH = 3
+  , MIN_CLUTCH = 1
+  , SPEED = 0.8
+  , ACC = 1.2;
