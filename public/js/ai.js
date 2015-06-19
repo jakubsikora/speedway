466e6978e1e641cfc1f8dddd10c5a945502f5125
Jakub Sikora <jakub.sikora@pure360.com>
2015-06-19 17:08:41 +0100

    playing with ai

--- public/js/ai.js
+++ public/js/ai.js
@@ -5,14 +5,16 @@ var AI = function() {
     , thrust = false
     , clutch = false
     , clutchFactor = 1
-    , friction = FRICTION_FACTOR
+    , friction = FRICTION_FACTOR * 10
     , angle = 0
     , angleVel = 0
     , sprite = null
     , img = new Image()
     , mapping = new Mapping()
     , mask = new Mask(FRAME_HEIGHT / 2)
-    , waypoints;
+    , waypoints
+    , line = new createjs.Shape()
+    , nextLine = new createjs.Shape();
 
   function init() {
     img.onload = handleImageLoad;
@@ -46,25 +48,107 @@ var AI = function() {
 
     mask.init(sprite.x, sprite.y);
     waypoints = waypoint.getWaypoints();
+    stage.addChild(line);
+    stage.addChild(nextLine);
+  }
+
+  function angleToVector(newAngle) {
+    return [Math.cos(newAngle), Math.sin(newAngle)];
   }
 
   function update() {
+    if (!sprite) return;
     //console.log(waypoint.getWaypoints());
-    sprite.x += 0.2;
+    //sprite.x += 1;
+    //sprite.x += 0.2;
+    velXY[0] *= (1 - friction);
+    velXY[1] *= (1 - friction);
+
+    angle += angleVel;
+
+    if (angle >= MAX_ANGLE || angle <= -MAX_ANGLE) {
+      angle = 0;
+    }
+
+    forward = angleToVector(angle);
+
+    velXY[0] += forward[0] * SPEED;
+    velXY[1] += forward[1] * SPEED;
+
+    sprite.x += velXY[0];
+    sprite.y += velXY[1];
+
     mask.update(sprite.x, sprite.y);
     findPoint();
   }
 
   function findPoint() {
+    var min = null
+      , distance
+      , dx
+      , dy
+      , waypoint
+      , index
+      , next;
+
     if (!waypoints) return;
 
-    var temp = waypoints[0];
+    waypoints.forEach(function(item) {
+      dx = sprite.x - item.x;
+      dy = sprite.y - item.y;
+      distance = Math.sqrt(dx * dx + dy * dy);
 
-    var dx = sprite.x - temp.x
-      , dy = sprite.y - temp.y
-      , distance = Math.sqrt(dx * dx + dy * dy);
+      if (!min) {
+        min = distance;
+        waypoint = item;
+      } else if (distance < min) {
+        min = distance;
+        waypoint = item;
+      }
+    });
+
+    index = waypoints.indexOf(waypoint);
+
+    if (index > waypoints.length) {
+      next = 0;
+    } else {
+      next = index + 1;
+    }
+
+    lookAtPoint(line, '#FF0000', waypoint);
+    lookAtPoint(nextLine, '#FFFF00', waypoints[next]);
+    moveToPoint(waypoints[next]);
+  }
+
+  function lookAtPoint(l, color, waypoint) {
+    l.graphics
+      .clear()
+      .setStrokeStyle(1)
+      .beginStroke(color)
+      .moveTo(sprite.x + (FRAME_WIDTH / 2), sprite.y)
+      .lineTo(waypoint.x, waypoint.y);
+  }
+
+  function moveToPoint(waypoint) {
+    var dx = sprite.x - waypoint.x
+      , dy = sprite.y - waypoint.y
+      , distance = Math.sqrt(dx * dx + dy * dy)
+      , maxDistance = 150
+      , minDistance = 50;
 
     console.log(distance);
+
+    if (distance > maxDistance) {
+      console.log('turn left');
+      angleVel = -TURN_ANGLE;
+      sprite.reverse();
+    } else if(distance < minDistance) {
+      console.log('turn right');
+      angleVel = TURN_ANGLE;
+      sprite.forward();
+    } else {
+      angleVel = 0;
+    }
 
   }
 
